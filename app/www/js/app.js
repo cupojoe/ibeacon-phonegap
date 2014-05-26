92695592c9050ef087dffd94d56ad7192ba8c8fc
cupojoe <joe.fraga@gmail.com>
2014-05-25 23:15:27 -0400

    Renamed the index.js to app.js

---
+++ app/www/js/app.js
@@ -0,0 +1,52 @@
+define([
+    'jquery',
+    'userController',
+    'userView',
+    'beaconController'
+    ], function($, userController, userView, beaconController) {
+
+        // Application Constructor
+        var initialize = function() {
+            bindEvents();
+        };
+        // Bind Event Listeners
+        //
+        // Bind any events that are required on startup. Common events are:
+        // 'load', 'deviceready', 'offline', and 'online'.
+        var bindEvents = function() {
+            //if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/)) {
+                document.addEventListener('deviceready', onDeviceReady, false);
+            /*} else {
+                window.device = {
+                    deviceid: "71EF5A2C-44A8-4F2C-BB75-F023752AEA70"
+                };
+                onDeviceReady();
+            }*/
+        };
+
+        // deviceready Event Handler
+        var onDeviceReady = function() {
+            if (parseInt(window.device.version) === 7) {
+                $('body').addClass('ios7');
+            }
+            userController.getUser(window.device.uuid, onUserFound, onUserGetError);
+        };
+
+        var onUserFound = function(res) {
+            userView.init(res);
+            userView.viewIn();
+            userView.populateFormControl();
+            beaconController.init();
+        };
+
+        var onUserGetError = function(err) {
+            console.log(err);
+            userView.prepareForEdit();
+            userView.viewIn();
+        };
+
+        return {
+            init: initialize
+        }
+    }
+);
